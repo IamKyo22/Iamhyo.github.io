f3b87f39c6399395a04478f67bb9cf20e90b5a83
Kyo <davifelippedias@Gmail.com>
2024-07-30 17:49:08 -0300

    Atualize o login OAuth2 para Discord e a página de callback

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-const CLIENT_ID = '1267914396990963815';
-const CLIENT_SECRET = 'SEU_CLIENT_SECRET'; // Insira o Client Secret aqui
+const CLIENT_ID = '1267914396990963815';  // Seu Client ID
+const CLIENT_SECRET = 'xxS2v6LWvt__mG_cpfO9ZyqtWMzKvb84';  // Seu Client Secret
 const REDIRECT_URI = 'https://iamkyo22.github.io/Iamhyo.github.io/';
 
 app.post('/exchange-token', async (req, res) => {
@@ -18,7 +18,7 @@ app.post('/exchange-token', async (req, res) => {
             'grant_type': 'authorization_code',
             'code': code,
             'redirect_uri': REDIRECT_URI,
-            'scope': 'identify'
+            'scope': 'email dm_channels.read'
         }), {
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
         });
